5867062c8d273fda18c04d8ffa1d43bffbf12614
Loïc <loic.boutet.1@ulaval.ca>
2024-09-09 20:09:45 -0400

    Second Commit 🐧🚨🙏💎🥳🤍😎👋☃️🌇🎛️🌊

--- untitled/src/main/java/org/example/Clinic.java
+++ untitled/src/main/java/org/example/Clinic.java
@@ -1,13 +1,43 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.example.VisibleSymptom.SPRAIN;
+
 public class Clinic {
-    public Clinic(/* ... */) {
+
+    private List<String> doctorQueue = new ArrayList<>();
+    private List<String> radiologyQueue = new ArrayList<>();
+    public Clinic() {
     }
 
     public void triagePatient(String name, int gravity, VisibleSymptom visibleSymptom) {
-        // TODO
+        doctorQueue.add(name);
+        if (isRadiologyPatient(visibleSymptom))
+        {
+            radiologyQueue.add(name);
+        }
     }
 
-    // D'autres méthodes peuvent être nécessaires
+    public String getNextDoctorPatient()
+    {
+        if (doctorQueue.isEmpty()) {
+            throw new EmptyQueueException();
+        }
+        return doctorQueue.remove(0);
+    }
 
+    public String getNextRadiologyPatient() {
+        if (radiologyQueue.isEmpty())
+        {
+            throw new EmptyQueueException();
+        }
+        return radiologyQueue.remove(0);
+    }
+
+    private boolean isRadiologyPatient(VisibleSymptom visibleSymptom)
+    {
+        return visibleSymptom == VisibleSymptom.BROKEN_BONE || visibleSymptom == SPRAIN;
+    }
 }
--- untitled/src/test/java/org/example/ClinicTest.java
+++ untitled/src/test/java/org/example/ClinicTest.java
@@ -1,7 +1,60 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClinicTest {
+
+    String A_NAME = "Monsieur Soleil";
+    String ANOTHER_NAME = "Docteur Queue";
+    int A_GRAVITY = 3;
+    VisibleSymptom A_NON_RADIOLOGY_SYMPTOM = VisibleSymptom.COLD;
+    VisibleSymptom A_RADIOLOGY_SYMPTOM = VisibleSymptom.SPRAIN;
+
+    @Test
+    void WhenCreatingClinic_ThenDoctorQueueShouldBeEmpty()
+    {
+        Clinic clinic = new Clinic();
+
+        assertThrows(EmptyQueueException.class, clinic::getNextDoctorPatient);
+    }
+
+    @Test
+    void WhenCreatingClinic_ThenRadiologyQueueShouldBeEmpty()
+    {
+        Clinic clinic = new Clinic();
+
+        assertThrows(EmptyQueueException.class, clinic::getNextRadiologyPatient);
+    }
+
+    @Test
+    void GivenMultiplePatients_WhenGetNextDoctorPatient_ThenGetFirstPatientTriaged(){
+        Clinic clinic = new Clinic();
+        clinic.triagePatient(A_NAME, A_GRAVITY, A_NON_RADIOLOGY_SYMPTOM);
+        clinic.triagePatient(ANOTHER_NAME, A_GRAVITY, A_NON_RADIOLOGY_SYMPTOM);
+
+        String nextPatient = clinic.getNextDoctorPatient();
+
+        assertEquals(A_NAME, nextPatient);
+    }
+
+    @Test
+    void GivenANonRadiologyPatient_WhenTriage_ThenRadiologyQueueShouldBeEmpty(){
+        Clinic clinic = new Clinic();
+
+        clinic.triagePatient(A_NAME, A_GRAVITY, A_NON_RADIOLOGY_SYMPTOM);
+
+        assertThrows(EmptyQueueException.class, clinic::getNextRadiologyPatient);
+    }
+
+    @Test
+    void GivenARadiologyPatient_WhenTriage_ThenPatientShouldBeNextInRadiologyQueue(){
+        Clinic clinic = new Clinic();
+
+        clinic.triagePatient(A_NAME, A_GRAVITY, A_RADIOLOGY_SYMPTOM);
+
+        assertEquals(A_NAME, clinic.getNextRadiologyPatient() );
+    }
 
 }
